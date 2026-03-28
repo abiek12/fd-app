7fa9dbf652c4dd7ba0cad8b442a1537ac71b7d42
Abhishek <abhishekkanichery@gmail.com>
2026-03-28 12:58:29 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="main-footer-section">
+    <footer className="main-footer-section py-[11.7rem] px-[3.7rem] flex justify-center items-center bg-off-white-bg">
       <div className="">This is footer section</div>
     </footer>
   );
