bcb8b1d879dc98f6dcd7b03e3318e512e3679c44
Abhishek <abhishekkanichery@gmail.com>
2026-04-10 10:45:21 +0530

    styled right section

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -34,46 +34,53 @@ const Cart = () => {
           </button>
         </div>
       </div>
-      <div className="right-container bg-white px-8 py-10 w-1/4 flex flex-col gap-6">
-        <div className="delivery-address flex flex-col gap-2">
-          <div className="heading flex justify-between items-center">
-            <h1 className="text-lg font-bold">Delivery Address</h1>
-            <PencilIcon size={20} color="#ff5200" className="cursor-pointer" />
+      <div className="right-container w-1/4 flex flex-col gap-6">
+        <div className="bg-white px-8 py-10 flex flex-col gap-6">
+          <div className="delivery-address flex flex-col gap-2">
+            <div className="heading flex justify-between items-center">
+              <h1 className="text-lg font-bold">Delivery Address</h1>
+              <PencilIcon
+                size={20}
+                color="#ff5200"
+                className="cursor-pointer"
+              />
+            </div>
+            <div className="address">
+              <div className="address-name text-lg font-semibold">Home</div>
+              <div className="building-area text-sm text-gray-600">
+                Puthiyapurayil House, Parassinikadavu, Kerala, India
+              </div>
+            </div>
+            <div className="estimated-time text-sm font-medium">21 MINS</div>
           </div>
-          <div className="address">
-            <div className="address-name text-lg font-semibold">Home</div>
-            <div className="building-area text-sm text-gray-600">
-              Puthiyapurayil House, Parassinikadavu, Kerala, India
+          <div className="apply-coupon flex justify-start items-center gap-3 p-4 border border-dashed border-gray-400 cursor-pointer">
+            <SealPercentIcon size={24} color="#77767b" />
+            <p className="text-sm text-gray-700 font-medium">Apply Coupon</p>
+          </div>
+          <div className="bill-details flex flex-col gap-2">
+            <h4 className="text-sm font-semibold">Bill Details</h4>
+            <div className="item-total flex justify-between items-center text-sm text-gray-500 font-normal">
+              <p className="">Item Total</p>
+              <p className="">₹250</p>
+            </div>
+            <div className="delivery-fee flex justify-between items-center text-sm text-gray-500 font-normal">
+              <p>Delivery Fee | 2.1 kms</p>
+              <p>₹51</p>
+            </div>
+            <hr className="my-2 border-t border-dashed border-gray-300" />
+            <div className="gst-other-charges flex justify-between items-center text-sm text-gray-500 font-normal">
+              <p>GST & Other Charges</p>
+              <p>₹50</p>
+            </div>
+            <hr className="my-3 border-t-2 border-gray-600" />
+            <div className="grand-total flex justify-between items-center text-sm font-semibold">
+              <p>TO PAY</p>
+              <p>₹351</p>
             </div>
           </div>
-          <div className="estimated-time text-sm font-medium">21 MINS</div>
         </div>
-        <div className="apply-coupon flex justify-start items-center gap-3 p-4 border border-dashed border-gray-400 cursor-pointer">
-          <SealPercentIcon size={24} color="#77767b" />
-          <p className="text-sm text-gray-700 font-medium">Apply Coupon</p>
-        </div>
-        <div className="bill-details">
-          <h4>Bill Details</h4>
-          <div className="item-total">
-            <p>Item Total</p>
-            <p>₹250</p>
-          </div>
-          <div className="delivery-fee">
-            <p>Delivery Fee | 2.1 kms</p>
-            <p>₹51</p>
-          </div>
-          <div className="gst-other-charges">
-            <p>GST & Other Charges</p>
-            <p>₹50</p>
-          </div>
-          <hr />
-          <div className="grand-total">
-            <p>TO PAY</p>
-            <p>₹351</p>
-          </div>
-          <div className="savings">
-            <h4>Savings of ₹25</h4>
-          </div>
+        <div className="savings p-4 bg-green-200 border border-solid border-green-600 text-green-800 font-medium">
+          <h4>Savings of ₹25</h4>
         </div>
       </div>
     </div>
