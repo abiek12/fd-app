d232429d2fdbd6a37b48674766e72a4922831529
Abhishek <abhishekkanichery@gmail.com>
2026-04-14 09:11:51 +0530

    register rtk query in the store

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
+import { api } from "./apis/api";
 
 const appStore = configureStore({
   reducer: {
-    cart: cartReducer,
+    [api.reducerPath]: api.reducer, // RTK Query
+    cart: cartReducer, // Local ui states
   },
+  middleware: (gdm) => gdm().concat(api.middleware),
 });
 
 export default appStore;
