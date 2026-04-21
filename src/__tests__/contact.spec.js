dbbdf324e6887c070f6e5ec4095e1fd75dde2d5a
Abhishek <abhishekkanichery@gmail.com>
2026-04-21 22:34:08 +0530

    grouping testcases and use 'it' aliases

--- src/__tests__/contact.spec.js
+++ src/__tests__/contact.spec.js
@@ -2,10 +2,16 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Contact from "../components/Contact";
 
-test("Should load about-us component", () => {
-  render(<Contact />);
+describe("Contact Us Page Test Cases", () => {
+  test("Should load about-us component", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
 
-  const heading = screen.getByRole("heading");
-
-  expect(heading).toBeInTheDocument();
+  it("Should load input name inside the contact component", () => {
+    render(<Contact />);
+    const inputBoxes = screen.getAllByRole("textbox");
+    expect(inputBoxes.length).toBe(1);
+  });
 });
