223e7ae2856641a0925f42839a04c015bea28752
Abhishek <abhishekkanichery@gmail.com>
2026-04-22 07:40:27 +0530

    update header test cases

--- src/__tests__/header.spec.js
+++ src/__tests__/header.spec.js
@@ -1,11 +1,12 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { Header } from "../components/Header";
 import appStore from "../utils/appStore";
 import { Provider } from "react-redux";
+import "@testing-library/jest-dom";
 
 describe("Header component test cases:", () => {
-  it("Should load header component with all items", () => {
+  it("Should load header component with all links", () => {
     render(
       <BrowserRouter>
         <Provider store={appStore}>
@@ -13,5 +14,7 @@ describe("Header component test cases:", () => {
         </Provider>
       </BrowserRouter>
     );
+    const items = screen.getAllByRole("link");
+    expect(items.length).toBe(4);
   });
 });
